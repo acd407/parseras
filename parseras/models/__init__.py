6015db74eed2523f272fde0d8f77f8176ecd6292
acd407 <acd407@qq.com>
2026-04-11 10:56:21 +0800

    feat(models): 添加StorageAreaModel等三个模型类到__all__
    将StorageAreaModel、BCLineModel和ConnectionModel添加到__all__列表，使其可以被外部导入

--- parseras/models/__init__.py
+++ parseras/models/__init__.py
@@ -2,5 +2,8 @@ from .cross_section import CrossSectionModel
 from .river import RiverModel
 from .lateral_weir import LateralWeirModel
 from .breakline import BreakLineModel
+from .storage_area import StorageAreaModel
+from .bcline import BCLineModel
+from .connection import ConnectionModel
 
-__all__ = ["CrossSectionModel", "RiverModel", "LateralWeirModel", "BreakLineModel"]
+__all__ = ["CrossSectionModel", "RiverModel", "LateralWeirModel", "BreakLineModel", "StorageAreaModel", "BCLineModel", "ConnectionModel"]
